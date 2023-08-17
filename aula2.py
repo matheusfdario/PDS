7e79a1f0725e586889c74d723e2c8b60ba0041be
Matheus Fortunato Dário <30605849+matheusfdario@users.noreply.github.com>
2023-08-17 17:08:28 -0300

    end of class 2

--- aula2.py
+++ aula2.py
@@ -4,18 +4,36 @@ Spyder Editor
 
 Autor: Matheus Fortunato Dário.
 Criação: 17/08/2023.
+1. Implementar a eq. a diferenças.
+2. Calcular numericamente respostas ao impolso.
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
 import pds
 
-n = np.arange(0,31)     #Gera um vetor de numpy com 31 posições, de 0 até 30.
-w0 = (np.pi)/10     
-x = np.cos(w0*n)
-plt.figure()
-plt.stem(n, x)
-plt.figure()
-plt.stem(n,pds.delta(n))
-plt.figure()
-plt.stem(n,pds.u(n))
+# Média Móvel
+M = 5
+n = np.arange(-3,10)
+x = pds.delta(n)
+b = np.ones(M+1)/(M+1)
+a = np.ones(1)
+h_ref = pds.lfilter(b, a, x)
+h = pds.eqdif(b, a, x)
+plt.figure(1)
+plt.stem(n,h_ref)
+plt.figure(2)
+plt.stem(n,h)
+
+print("RES1:",pds.mse(h_ref, h))
+
+#Sistema IIR
+b = np.ones(1)
+a = np.array([1,.5])
+h_ref = pds.lfilter(b, a, x)
+h = pds.eqdif(b, a, x)
+plt.figure(3)
+plt.stem(n,h_ref)
+plt.figure(4)
+plt.stem(n,h)
+print("RES2:",pds.mse(h_ref, h))
--- pds.py
+++ pds.py
@@ -8,8 +8,24 @@ Criação: 10/08/2023.
 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.signal import lfilter
 
-def delta(n):           #função do impulso unitário
+def mse(x, y):          # diferença média quadrática
+    return np.mean(np.abs(x-y)**2)    
+
+def delta(n):           #função do impulso discreto
     return 1.*(n==0)
-def u(n):               #função do impulso unitário
+def u(n):               #função do degrau unitário
     return 1.*(n>=0)
+def eqdif(b, a, x):
+    y = np.zeros_like(x)    #cria variável para saída
+    M = len(b)
+    N = len(a)
+    for n in range(len(y)):
+        for kb in range(M):    
+            if((n-kb)>=0):
+                y[n] += b[kb]*x[n-kb]
+        for ka in range(1,N):
+            if((n-kb)>=0):
+                y[n] += -1*(a[ka]*y[n-ka]) 
+    return y
